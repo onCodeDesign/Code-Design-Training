04b7e908a6a38dd4e2b2025827f9b2bded0504f1
Florin <florin.coros@gmail.com>
2014-11-14 17:30:00 +0200

    AppBoot: fix build by adding IRepositoryExceptionHandler

---
+++ AppBoot/Infrastructure/DataAccess/EfRepositoryExceptionHandler/IRepositoryExceptionHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DataAccess.Exceptions;
+
+namespace DataAccess.EfRepositoryExceptionHandler
+{
+    interface IRepositoryExceptionHandler
+    {
+        void Handle(Exception exception);
+    }
+
+    class RepositoryExceptionHandler : IRepositoryExceptionHandler
+    {
+        private readonly IRepositoryExceptionHandler chainHead =
+            new RepositorySqlExceptionHandler(
+                new RepositoryConcurrencyExceptionHandler(
+                    new RepositoryUpdateExceptionHandler(
+                        new RepositoryDbEntityValidationExceptionHandler(
+                            new RepositoryDefaultExceptionHandler())))
+                );
+
+        public void Handle(Exception exception)
+        {
+            chainHead.Handle(exception);
+        }
+    }
+}
