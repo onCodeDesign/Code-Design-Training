35c9698d508c0b42ee22c7e1efa0e62996cf0212
Florin <florin.coros@iquarc.com>
2015-08-31 16:00:54 +0300

    add Common project

--- AppInfraDemo/UI/WpfApplication/MainWindow.xaml.cs
+++ AppInfraDemo/UI/WpfApplication/MainWindow.xaml.cs
@@ -1,28 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Contracts.Location;
+using Microsoft.Practices.ServiceLocation;
 
 namespace WpfApplication
 {
 	/// <summary>
-	/// Interaction logic for MainWindow.xaml
+	///     Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window
 	{
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			ILocationService service = ServiceLocator.Current.GetInstance<ILocationService>();
+			service.GetCoordinates("some city", "street name", "house number");
 		}
 	}
 }
