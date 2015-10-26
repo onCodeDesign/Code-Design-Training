469a551175f809a4870de0531400bec3939c85b2
Florin <florin.coros@iquarc.com>
2015-10-26 16:03:20 +0200

    patterns: upgrade to .NET 4.6 & C# 6; improve Composite examples

--- DesignPatterns/ConsoleDemo/Composite/00_Client.cs
+++ DesignPatterns/ConsoleDemo/Composite/00_Client.cs
@@ -2,72 +2,78 @@
 
 namespace ConsoleDemo.Composite.Transparency
 {
-    public static class CompositeClient
-    {
-        public static void Demo()
-        {
-            //Demo1();
+	public static class CompositeClient
+	{
+		public static void Demo()
+		{
+			//Demo1();
 
-            Demo2();
-        }
+			Demo2();
+		}
 
-        private static void Demo1()
-        {
-            var drawing = BuildDrawing();
-            drawing.Draw();
-        }
+		private static void Demo1()
+		{
+			var drawing = BuildDrawing();
+			drawing.Draw();
+		}
 
-        private static void Demo2()
-        {
-            Console.WriteLine("Build a drawing ...");
-            var drawing = BuildDrawing();
-            drawing.Draw();
+		private static void Demo2()
+		{
+			Console.WriteLine("Build a drawing  ...");
+			var drawing = BuildDrawing();
+			drawing.Draw();
 
-            Console.WriteLine("\n----------- Edit the drawing -----------");
-            Console.WriteLine("adding a text to all elements which contain 'great' ");
+			Console.WriteLine("\n----------- Edit the drawing -----------");
+			Console.WriteLine("adding a text to all elements which contain 'great' ");
 
-            GraphicText text = new GraphicText("This is a nice graphic", ConsoleColor.DarkBlue);
+			GraphicText text = new GraphicText("This is a nice graphic", ConsoleColor.DarkBlue);
 
-            foreach (IGraphicElement element in drawing.GetChildElements())
-            {
-                if (element.Name.Contains("great"))
-                {
-                    element.Add(text);
-                }
-            }
+			AddElementWhenNameContains("great", text, drawing);
 
-            Console.WriteLine("\n-----------The drawing after edit ---------");
-            drawing.Draw();
-        }
+			Console.WriteLine("\n-----------The drawing after edit ---------");
+			drawing.Draw();
+		}
 
-        private static Drawing BuildDrawing()
-        {
-            var drawing = new Drawing
-            {
-                new GraphicText("My drawing title!"),
-                new Line("First Line"),
-                new Picture("great view")
-                {
-                    new GraphicText("A text over my great picture"),
-                    new Line("A line over my picture"),
-                    new Group
-                    {
-                        new Picture
-                        {
-                            new Line("A line over the picture"),
-                            new Line("Other line over the picture"),
-                        },
-                        new GraphicText("A text in the group")
-                    }
-                },
-                new Group
-                {
-                    new Picture(),
-                    new Line("line near the picture")
-                }
-            };
+		private static void AddElementWhenNameContains(string content, GraphicText text, IGraphicElement drawing)
+		{
+			foreach (IGraphicElement element in drawing.GetChildElements())
+			{
+				if (element.Name.Contains(content))
+					element.Add(text);
 
-            return drawing;
-        }
-    }
+				foreach (var child in element.GetChildElements())
+					AddElementWhenNameContains(content, text, child);
+			}
+		}
+
+		private static Drawing BuildDrawing()
+		{
+			var drawing = new Drawing
+			{
+				new GraphicText("My drawing title!"),
+				new Line("First Line"),
+				new Picture("great view")
+				{
+					new GraphicText("A text over my great picture"),
+					new Line("A line over my picture"),
+					new Group
+					{
+						new Picture
+						{
+							new Line("A line over the picture"),
+							new Line("Other line over the picture"),
+						},
+						new GraphicText("A text in the group")
+					}
+				},
+				new Group
+				{
+					new Picture(),
+					new Line("line near the picture")
+				}
+			};
+
+			return drawing;
+		}
+	}
 }
