fba49fbb6ea1be1dffeee3ab19c6c8b9980226fd
Florin <florin.coros@iquarc.com>
2016-03-26 16:00:32 +0200

    add Sales.DataAccess

---
+++ AppInfraDemo/Modules/Sales/Sales.DataAccess/DbContextFactory.cs
@@ -0,0 +1,10 @@
+﻿using iQuarc.AppBoot;
+using iQuarc.DataAccess;
+
+namespace Sales.DataAccess
+{
+	[Service(typeof(IDbContextFactory))]
+	public class DbContextFactory : DbContextFactory<SalesEntities>
+    {
+    }
+}
