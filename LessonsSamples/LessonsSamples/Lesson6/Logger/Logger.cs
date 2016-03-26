beeade615a87b3b3d7314f5b70320a07433d7c00
Florin <florin.coros@iquarc.com>
2016-03-26 12:42:45 +0200

    add Lesson6 \ Logger sample

---
+++ LessonsSamples/LessonsSamples/Lesson6/Logger/Logger.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Microsoft.Practices.ServiceLocation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace LessonsSamples.Lesson6.Logger
+{
+	public interface ILogSrv
+	{
+		void WriteTrace(Trace trace);
+	}
+
+	class Logger
+	{
+	}
+
+	[TestClass]
+	public class UnitTests
+	{
+		private Mock<IServiceLocator> slStub;
+
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			slStub = new Mock<IServiceLocator>();
+			ServiceLocator.SetLocatorProvider(() => slStub.Object);
+		}
+
+		[TestMethod]
+		public void PlaceNewOrder_FromPriorityCustomer_AddedOnTopOfTheQueue()
+		{
+			Mock<ILogSrv> dummyLog = new Mock<ILogSrv>();
+			
+			slStub.Setup(l => l.GetInstance<ILogSrv>()).Returns(dummyLog.Object);
+
+			//...
+		}
+	}
+}
