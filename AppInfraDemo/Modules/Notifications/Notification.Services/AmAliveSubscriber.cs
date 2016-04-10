9565fe672b2f8b85abb44181cda8cb99bd925177
Florin <florin.coros@iquarc.com>
2016-04-10 18:41:50 +0300

    [InfraEx 1.3] add AmAliveSubscriber open generic implementation

---
+++ AppInfraDemo/Modules/Notifications/Notification.Services/AmAliveSubscriber.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Contracts.Notifications;
+using iQuarc.AppBoot;
+using iQuarc.SystemEx;
+
+namespace Notifications
+{
+    [Service(typeof(IAmAliveSubscriber<>))]
+    class AmAliveSubscriber<T> : IAmAliveSubscriber<T>
+    {
+        readonly IEnumerable<IAmAliveSubscriber<T>> subscribers;
+
+        public AmAliveSubscriber(IAmAliveSubscriber<T>[] subscribers)
+        {
+            this.subscribers = subscribers;
+        }
+
+        public void AmAlive(T item)
+        {
+            subscribers.ForEach(s => s.AmAlive(item));
+        }
+    }
+}
