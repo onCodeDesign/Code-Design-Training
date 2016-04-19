ebc18d5bc39b43f6d53d74c69a16a4ccc5e39abf
Florin <florin.coros@iquarc.com>
2016-04-19 18:38:57 +0300

    Change exercises numbering and add some test data T-SQL queries

--- AppInfraDemo/UI/ConsoleApplication/AdventureWorksTestQueries/SalesOrdersHeader.sql
+++ AppInfraDemo/UI/ConsoleApplication/AdventureWorksTestQueries/SalesOrdersHeader.sql
@@ -5,3 +5,9 @@
 SELECT CustomerID, count(*) as Orders
   FROM [Sales].[SalesOrderHeader]
 GROUP BY CustomerID
+
+SELECT *
+				FROM [Sales].[SalesOrderHeader] SO 
+					INNER JOIN [Sales].[Customer] C ON SO.CustomerID=C.CustomerId
+					INNER JOIN [Person].[Person] P ON P.BusinessEntityID=C.PersonId
+			WHERE P.LastName='Abel'
