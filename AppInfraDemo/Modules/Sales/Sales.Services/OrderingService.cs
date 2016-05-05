5a256ad412c67a9c1738a64a5202cd613a542fc5
Florin <florin.coros@iquarc.com>
2016-05-05 14:54:38 +0300

    [AppInfra] fix in OrderingService

--- AppInfraDemo/Modules/Sales/Sales.Services/OrderingService.cs
+++ AppInfraDemo/Modules/Sales/Sales.Services/OrderingService.cs
@@ -52,12 +52,16 @@ namespace Sales
                 {
                     SalesOrderHeader order
                         = uof.GetEntities<SalesOrderHeader>()
-                                                .FirstOrDefault(o => o.CustomerID == c.CustomerID &&
-                                                                     o.OrderDate.Month == DateTime.Now.Month);
+                             .FirstOrDefault(o => o.CustomerID == c.CustomerID &&
+                             o.OrderDate.Month == DateTime.Now.Month);
 
                     if (order == null)
                     {
-                        order = new SalesOrderHeader {Customer = c};
+                        order = new SalesOrderHeader
+						{
+							Customer = c,
+							OrderDate = DateTime.Now.Date
+						};
                         uof.Add(order);
                     }
 
