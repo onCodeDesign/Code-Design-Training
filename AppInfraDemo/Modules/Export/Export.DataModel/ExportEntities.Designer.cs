82e52dad09c0a00144b638d65bdd53d44521ed45
Florin <florin.coros@iquarc.com>
2016-09-27 13:39:36 +0300

    [AppInfra] optimize projects references

--- AppInfraDemo/Modules/Export/Export.DataModel/ExportEntities.Designer.cs
+++ AppInfraDemo/Modules/Export/Export.DataModel/ExportEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Work\Training\OOD\Examples\AppBoot\Modules\Export\Export.DataModel\ExportEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'c:\D\Projects\iQuarc\Code-Design-Training\AppInfraDemo\Modules\Export\Export.DataModel\ExportEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
