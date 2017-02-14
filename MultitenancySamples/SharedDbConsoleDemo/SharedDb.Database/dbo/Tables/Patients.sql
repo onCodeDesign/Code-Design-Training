fabad73a5167031076eecddfdc446a321b80413c
Florin <florin.coros@iquarc.com>
2017-02-14 17:41:09 +0200

    [SharedDb] Change the database schema for multitenancy

--- MultitenancySamples/SharedDbConsoleDemo/SharedDb.Database/dbo/Tables/Patients.sql
+++ MultitenancySamples/SharedDbConsoleDemo/SharedDb.Database/dbo/Tables/Patients.sql
@@ -1,7 +1,11 @@
 ﻿CREATE TABLE [dbo].[Patients] (
     [ID]          INT           IDENTITY (1, 1) NOT NULL,
+    [TenantID]    INT           NULL,
     [Name]        NVARCHAR (50) NOT NULL,
     [DateOfBirth] DATETIME2 (7) NOT NULL,
-    CONSTRAINT [PK_Patients] PRIMARY KEY CLUSTERED ([ID] ASC)
+    CONSTRAINT [PK_Patients] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_Patients_Tenants] FOREIGN KEY ([TenantID]) REFERENCES [dbo].[Tenants] ([ID])
 );
+
+
 
