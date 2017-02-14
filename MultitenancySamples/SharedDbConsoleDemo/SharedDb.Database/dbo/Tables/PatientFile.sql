90fa49dbe05b71bc7a4a83da26c29e9323afe745
Florin <florin.coros@iquarc.com>
2017-02-14 15:14:01 +0200

    [SharedDb] Add the SharedDb.Database project w/ the initial DB schema for no multitenancy scenario

---
+++ MultitenancySamples/SharedDbConsoleDemo/SharedDb.Database/dbo/Tables/PatientFile.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[PatientFile] (
+    [ID]           INT           IDENTITY (1, 1) NOT NULL,
+    [PatientID]    INT           NOT NULL,
+    [CreationDate] DATETIME2 (7) NOT NULL,
+    CONSTRAINT [PK_PatientFile] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_PatientFile_Patients] FOREIGN KEY ([PatientID]) REFERENCES [dbo].[Patients] ([ID])
+);
+
---
+++ MultitenancySamples/SharedDbConsoleDemo/SharedDb.Database/dbo/Tables/Patients.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Patients] (
+    [ID]          INT           IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (50) NOT NULL,
+    [DateOfBirth] DATETIME2 (7) NOT NULL,
+    CONSTRAINT [PK_Patients] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
