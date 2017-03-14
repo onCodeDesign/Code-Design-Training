d58877315752bf4b37263fe798f850f6f7c2981f
Florin <florin.coros@iquarc.com>
2017-03-14 15:10:52 +0200

    [Samples][L8] Add sample for implementing Row-Level Authentification

--- LessonsSamples/LessonsSamples/Customer.cs
+++ LessonsSamples/LessonsSamples/Customer.cs
@@ -7,16 +7,40 @@ namespace LessonsSamples
     {
         public string Name { get; set; }
         public Address Address { get; set; }
-        ICollection<Customer> Cusotmers { get; set; }
+        ICollection<Order> Orders { get; set; }
         public string CompanyName { get; set; }
+        public int AreadID { get; set; }
     }
 
     public class Order
     {
+        public int ID { get; set; }
         public string Description { get; set; }
         public Customer Customer { get; set; }
         public decimal ApprovedAmmount { get; set; }
         public DateTime OrderDate { get; set; }
+        public ICollection<OrderLine> OrderLines { get; set; }
+    }
+
+    public class OrderLine
+    {
+        public int OrderID;
+        public Product Product { get; set; }   
+    }
+
+    public class Product
+    {
+        public Producer Producer { get; set; }
+    }
+
+    public class Producer
+    {
+        public int AreaID;
+    }
+
+    internal class SalesArea
+    {
+        public string CountryCode { get; set; }
     }
 
     public class Address
---
+++ LessonsSamples/LessonsSamples/Lesson8/RowLevelAuth/IRowAuthPoliciesContainer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace LessonsSamples.Lesson8.RowLevelAuth
+{
+    interface IRowAuthPoliciesContainer
+    {
+        bool HasPolicy<TEntity>();
+        IRowAuthPolicy<TEntity> GetPolicy<TEntity>();
+        RowAuthPolicy<TEntity, TProperty> Register<TEntity, TProperty>(Expression<Func<TEntity, TProperty>> selector);
+    }
+}
