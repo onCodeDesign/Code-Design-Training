b584d7f2f66fc371d4b4fdb488561f385116717d
Florin <florin.coros@iquarc.com>
2017-04-18 15:09:58 +0300

    Make initial implementation of ConsoleHost using ASP.NET Web API Owin SelfHost

--- InterProcessCommunication/TradingApp/ConsoleHost/Program.cs
+++ InterProcessCommunication/TradingApp/ConsoleHost/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
 
 namespace ConsoleHost
 {
@@ -10,6 +11,20 @@ namespace ConsoleHost
     {
         static void Main(string[] args)
         {
+            string baseAddress = "http://localhost:9000/";
+
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                Console.WriteLine($"Server runs at: {baseAddress}");
+                Console.WriteLine("Press ESC to stop the server\n");
+
+                ConsoleKeyInfo keyInfo;
+                do
+                {
+                    keyInfo = Console.ReadKey();
+                }
+                while (keyInfo.Key != ConsoleKey.Escape);
+            }
         }
     }
 }
