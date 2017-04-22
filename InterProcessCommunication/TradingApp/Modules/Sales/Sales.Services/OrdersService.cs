e7de4291e5a0ac66a521fee9153b73bfe0b8031f
Florin <florin.coros@iquarc.com>
2017-04-22 12:29:09 +0300

    Refactor the OrderingService to use a Repository to store the orders

--- InterProcessCommunication/TradingApp/Modules/Sales/Sales.Services/OrdersService.cs
+++ InterProcessCommunication/TradingApp/Modules/Sales/Sales.Services/OrdersService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Contracts.Quotations.Services;
 using Contracts.Sales.Services;
@@ -8,23 +9,25 @@ using iQuarc.AppBoot;
 namespace Sales.Services
 {
     [Service(typeof(IOrdersService))]
-    public class OrderingService : IOrdersService
+    class OrderingService : IOrdersService
     {
         private readonly IQuotationService quotationService;
+        private readonly IRepository repository;
 
-        private readonly List<LimitOrder> limitOrders = new List<LimitOrder>();
-
-        public OrderingService(IQuotationService quotationService)
+        public OrderingService(IQuotationService quotationService, IRepository repository)
         {
             this.quotationService = quotationService;
+            this.repository = repository;
         }
 
         public void PlaceSellLimitOrder(string securityCode, decimal sellingPrice, DateTime validUntil)
         {
+            List<LimitOrder> limitOrders = new List<LimitOrder>();
             var todayQuotations = quotationService.GetQuotations(securityCode, DateTime.Today.AddDays(-1), DateTime.Today);
             foreach (var quotation in todayQuotations)
             {
                 if (quotation.AskPrice >= sellingPrice)
+                {
                     limitOrders.Add(new LimitOrder
                     {
                         SecurityCode = securityCode,
@@ -33,15 +36,21 @@ namespace Sales.Services
                         Price = sellingPrice,
                         ValidUntil = validUntil
                     });
+                }
             }
+
+            repository.SaveAll(limitOrders);
         }
 
         public void PlaceBuyLimitOrder(string securityCode, decimal buyingPrice, DateTime validUntil)
         {
+            List<LimitOrder> limitOrders= new List<LimitOrder>();
             var todayQuotations = quotationService.GetQuotations(securityCode, DateTime.Today.AddDays(-1), DateTime.Today);
             foreach (var quotation in todayQuotations)
             {
                 if (quotation.BidPrice <= buyingPrice)
+                {
+
                     limitOrders.Add(new LimitOrder
                     {
                         SecurityCode = securityCode,
@@ -50,12 +59,15 @@ namespace Sales.Services
                         Price = buyingPrice,
                         ValidUntil = validUntil
                     });
+                }
             }
+
+            repository.SaveAll(limitOrders);
         }
 
         public LimitOrder[] GetLimitOrders()
         {
-            return limitOrders.ToArray();
+            return repository.GetEntities<LimitOrder>().ToArray();
         }
     }
 }
