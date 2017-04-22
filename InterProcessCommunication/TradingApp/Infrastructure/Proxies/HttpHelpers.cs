0517b26f3fd25d0baeda31ec614a5cbf4749c05f
Florin <florin.coros@iquarc.com>
2017-04-22 14:49:54 +0300

    Create Proxies project

---
+++ InterProcessCommunication/TradingApp/Infrastructure/Proxies/HttpHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Proxies
+{
+    public static class HttpHelpers
+    {
+        public static HttpClient CreateNewClient<T>()
+        {
+            string address = GetAddress<T>();
+            var client = new HttpClient();
+            client.BaseAddress=new Uri(address);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return client;
+        }
+
+        public static string GetAddress<T>()
+        {
+            string settingKey = $"{typeof(T).Name}_Address";
+            return ConfigurationManager.AppSettings[settingKey];
+        }
+    }
+}
