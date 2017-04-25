ad69c1b84a33d8936597f6f390b73b3cc78111a5
Florin <florin.coros@iquarc.com>
2017-04-25 12:38:37 +0300

    Improve request logging for the ConsoleHost

--- InterProcessCommunication/TradingApp/Infrastructure/Proxies/HttpHelpers.cs
+++ InterProcessCommunication/TradingApp/Infrastructure/Proxies/HttpHelpers.cs
@@ -2,11 +2,15 @@
 using System.Configuration;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Web.UI;
 
 namespace Proxies
 {
     public static class HttpHelpers
     {
+        public static readonly Guid ClientId = Guid.NewGuid();
+
         public static HttpClient CreateNewClient<T>()
         {
             string address = GetBaseAddress<T>();
@@ -14,6 +18,7 @@ namespace Proxies
             client.BaseAddress = new Uri(address);
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("Service-Proxy", ClientId.ToString());
             return client;
         }
 
