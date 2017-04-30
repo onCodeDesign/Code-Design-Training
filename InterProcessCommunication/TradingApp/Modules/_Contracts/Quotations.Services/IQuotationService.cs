a9d5b6bf2b094fbb52a4edf2b9a0e15356e3f1a4
Florin <florin.coros@iquarc.com>
2017-04-30 11:21:54 +0300

    Create IQuotationService contracts

---
+++ InterProcessCommunication/TradingApp/Modules/_Contracts/Quotations.Services/IQuotationService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Contracts.Quotations.Services
+{
+    public interface IQuotationService
+    {
+        Quotation[] GetQuotations(string exchange, string instrument, DateTime from, DateTime to);
+        Quotation[] GetQuotations(string securityCode, DateTime from, DateTime to);
+    }
+}
---
+++ InterProcessCommunication/TradingApp/Modules/_Contracts/Quotations.Services/Quotation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Contracts.Quotations.Services
+{
+    public class Quotation
+    {
+        public DateTime Timestamp { get; set; }
+        public decimal BidPrice { get; set; }
+        public decimal AskPrice { get; set; }
+        public string SecurityCode { get; set; }
+    }
+}
