08b24ce25614133f35854931e9283d604e70d809
Florin <florin.coros@iquarc.com>
2017-04-30 15:22:42 +0300

    Implement ServiceProxyAttribute and ServiceProxyRegistrationBehavior

---
+++ InterProcessCommunication/TradingApp/AppBootEx/ServiceProxyRegistrationBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using iQuarc.AppBoot;
+using iQuarc.SystemEx;
+
+namespace AppBootEx
+{
+    public sealed class ServiceProxyRegistrationBehavior : IRegistrationBehavior
+    {
+        public IEnumerable<ServiceInfo> GetServicesFrom(Type type)
+        {
+            IEnumerable<ServiceProxyAttribute> attributes = type.GetAttributes<ServiceProxyAttribute>(false);
+            return attributes.Select(a => new ServiceInfo(a.ExportType, type, string.Empty, Lifetime.AlwaysNew));
+        }
+    }
+}
