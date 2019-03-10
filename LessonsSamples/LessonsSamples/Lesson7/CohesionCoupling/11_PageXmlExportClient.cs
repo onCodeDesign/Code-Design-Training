6b89ff299ebec24e7045c649192de9762b9dea8c
Florin Coros <florin.coros@iquarc.com>
2019-03-10 11:45:21 +0200

    [Samples][L7] Add client code for Cohesion refactoring example

---
+++ LessonsSamples/LessonsSamples/Lesson7/CohesionCoupling/11_PageXmlExportClient.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using CommonServiceLocator;
+
+namespace LessonsSamples.Lesson7.CohesionCoupling
+{
+    class PageXmlExportClient_1
+    {
+        public void ExportDataForCustomers(IEnumerable<string> customers)
+        {
+            var exporter = new PageXmlExport_1("{0}_{1}_{2}", true, 10, true);
+
+            foreach (var customer in customers)
+            {
+                exporter.ExportCustomerPage(customer);
+            }
+        }
+    }
+
+    class PageXmlExportClient_12
+    {
+        public void ExportDataForCustomers(IEnumerable<string> customers)
+        {
+            var exporter = new PageXmlExport_1("{0}_{1}_{2}", true, 10, true);
+            ICrmService crmService = ServiceLocator.Current.GetInstance<ICrmService>();
+            ILocationService locationService = ServiceLocator.Current.GetInstance<ILocationService>();
+
+            foreach (var customer in customers)
+            {
+                PageData userInput = GetUserInput(customer);
+                exporter.ExportCustomerPageWithExternalData(customer, userInput, crmService, locationService);
+            }
+        }
+
+        private PageData GetUserInput(string customer)
+        {
+            // TODO: take this data from a view model
+            return new PageData();
+        }
+    }
+}
---
+++ LessonsSamples/LessonsSamples/Lesson7/CohesionCoupling/13_PageXmlExportClient.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using CommonServiceLocator;
+
+namespace LessonsSamples.Lesson7.CohesionCoupling
+{
+    class PageXmlExportClient_3
+    {
+        public void ExportDataForCustomers(IEnumerable<string> customers)
+        {
+            ICrmService crmService = ServiceLocator.Current.GetInstance<ICrmService>();
+            ILocationService locationService = ServiceLocator.Current.GetInstance<ILocationService>();
+            IPageFileWriter pageWriter = ServiceLocator.Current.GetInstance<IPageFileWriter>();
+
+            var exporter = new PageXmlExport_3(pageWriter, 10, true, crmService, locationService);
+
+            foreach (var customer in customers)
+            {
+                PageData userInput = GetUserInput(customer);
+                exporter.ExportCustomerPageWithExternalData(customer, userInput);
+            }
+        }
+
+        private PageData GetUserInput(string customer)
+        {
+            // TODO: take this data from a view model
+            return new PageData();
+        }
+    }
+}
