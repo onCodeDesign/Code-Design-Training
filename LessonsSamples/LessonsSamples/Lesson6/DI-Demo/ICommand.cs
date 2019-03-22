9ce17bd014a7dcf2c7560cf5879c03fd30714840
Florin Coros <florin.coros@iquarc.com>
2019-03-22 11:57:17 +0200

    Do ex 1

---
+++ LessonsSamples/LessonsSamples/Lesson6/DI-Demo/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LessonsSamples.Lesson6
+{
+    public interface ICommand
+    {
+        void Execute();
+        char KeyChar { get; }
+        string MenuEntry { get; }
+    }
+}
