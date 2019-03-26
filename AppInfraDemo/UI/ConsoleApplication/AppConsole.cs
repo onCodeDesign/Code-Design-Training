f279b13e75aefb9ea2d0761493fe810f0b829070
Florin Coros <florin.coros@iquarc.com>
2019-03-26 18:55:46 +0200

    [AppInfra] Add more methods to IConsole and update the exercises

--- AppInfraDemo/UI/ConsoleApplication/AppConsole.cs
+++ AppInfraDemo/UI/ConsoleApplication/AppConsole.cs
@@ -1,37 +1,53 @@
 using System;
+using System.Runtime.InteropServices;
 using iQuarc.AppBoot;
 using iQuarc.SystemEx;
 
 namespace ConsoleApplication
 {
-	[Service(typeof(IConsole), Lifetime.Application)]
-	class AppConsole : IConsole
-	{
-		public string AskInput(string message)
-		{
-			Console.WriteLine();
-			Console.WriteLine(message);
+    [Service(typeof(IConsole), Lifetime.Application)]
+    class AppConsole : IConsole
+    {
+        public string AskInput(string message)
+        {
+            Console.WriteLine();
+            Console.WriteLine(message);
 
-			return Console.ReadLine();
-		}
+            return Console.ReadLine();
+        }
 
-		public void WriteEntity<T>(T entity)
-		{
-			Console.WriteLine();
-			Console.WriteLine($"--------------- {typeof(T).Name} ----------------");
-			var properties = ReflectionExtensions.GetEditableSimpleProperties(entity);
-			foreach (var propertyInfo in properties)
-			{
-				Console.Write($"{propertyInfo.Name}: ");
-				Console.WriteLine(propertyInfo.GetValue(entity));
-			}
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
 
-			Console.WriteLine("-----------------------------------------------------");
-		}
+        public ConsoleKeyInfo ReadKey()
+        {
+            return Console.ReadKey();
+        }
 
-		public void WriteLine(string line)
-		{
-			Console.WriteLine(line);
-		}
-	}
+        public void WriteEntity<T>(T entity)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"--------------- {typeof(T).Name} ----------------");
+            var properties = ReflectionExtensions.GetEditableSimpleProperties(entity);
+            foreach (var propertyInfo in properties)
+            {
+                Console.Write($"{propertyInfo.Name}: ");
+                Console.WriteLine(propertyInfo.GetValue(entity));
+            }
+
+            Console.WriteLine("-----------------------------------------------------");
+        }
+
+        public void WriteLine(string line)
+        {
+            Console.WriteLine(line);
+        }
+
+        public void Clear()
+        {
+            Console.Clear();
+        }
+    }
 }
