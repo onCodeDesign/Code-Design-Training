73715c4e9194c5a9b8651b79e333c40c04a4b279
Florin Coros <florin.coros@iquarc.com>
2019-07-03 17:09:04 +0300

    [AppInfra] Add SalesOrderHeaderStatusValues

---
+++ AppInfraDemo/Modules/Sales/Sales.DataModel/Values/SalesOrderHeaderStatusValues.cs
@@ -0,0 +1,12 @@
+﻿namespace Sales.DataModel.Values
+{
+    public static class SalesOrderHeaderStatusValues
+    {
+        public const byte InProcess = 1;
+        public const byte Approved = 2;
+        public const byte Backordered = 3;
+        public const byte Rejected = 4;
+        public const byte Shipped = 5;
+        public const byte Cancelled = 6;
+    }
+}
