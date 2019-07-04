7c691f2bcd4afa515bdb99a7014c76d2627df4b8
Florin Coros <florin.coros@iquarc.com>
2019-07-04 16:19:01 +0300

    Do Ex 1.1

---
+++ AppInfraDemo/Modules/Notifications/Notification.Services/NotificationsModule.cs
@@ -0,0 +1,23 @@
+﻿using Contracts.Notifications;
+using iQuarc.AppBoot;
+using iQuarc.SystemEx.Priority;
+
+namespace Notifications
+{
+    [Priority(Priorities.High)]
+    [Service(nameof(NotificationsModule), typeof(IModule))]
+    class NotificationsModule :IModule
+    {
+        private readonly INotificationService notSrv;
+
+        public NotificationsModule(INotificationService notSrv)
+        {
+            this.notSrv = notSrv;
+        }
+
+        public void Initialize()
+        {
+            notSrv.NotifyAlive((IModule) this);
+        }
+    }
+}
