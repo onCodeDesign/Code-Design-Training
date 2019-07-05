7831f93a49c562a2f72c34f71b55ded31186ea7b
Florin Coros <florin.coros@iquarc.com>
2019-07-05 14:23:30 +0300

    AppInfraDemo: Do ex 4.1

---
+++ AppInfraDemo/Modules/Sales/Sales.ConsoleUi/ShowCustomersCommand.cs
@@ -0,0 +1,34 @@
+﻿using Contracts.ConsoleUi;
+using Contracts.Sales.CustomerOrders;
+using iQuarc.AppBoot;
+
+namespace Sales.ConsoleUi
+{
+    [Service(nameof(ShowCustomersCommand), typeof(IConsoleCommand))]
+    class ShowCustomersCommand : IConsoleCommand
+    {
+        private readonly ICustomerOrdersService customersSrv;
+        private readonly IConsole console;
+
+        public ShowCustomersCommand(ICustomerOrdersService customersSrv, IConsole console)
+        {
+            this.customersSrv = customersSrv;
+            this.console = console;
+        }
+
+        public void Execute()
+        {
+            var customers = customersSrv.GetCustomersWithOrders();
+
+            console.WriteLine("The following customers were found:");
+            console.WriteLine(string.Empty);
+
+            foreach (var customer in customers)
+            {
+                console.WriteEntity(customer);
+            }
+        }
+
+        public string MenuLabel => "Show Customers";
+    }
+}
