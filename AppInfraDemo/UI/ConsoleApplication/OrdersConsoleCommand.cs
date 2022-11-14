bb191ac5f450c6d202253b3bb15cdce98f570a24
Florin Coros <florin@oncodedesign.com>
2022-11-14 16:11:50 +0200

    [InfraEx 3.1] Transform the OrdersConsoleApplication, in a `IConsoleCommand` implementation

--- AppInfraDemo/UI/ConsoleApplication/OrdersConsoleCommand.cs
+++ AppInfraDemo/UI/ConsoleApplication/OrdersConsoleCommand.cs
@@ -3,19 +3,19 @@ using iQuarc.AppBoot;
 
 namespace ConsoleApplication
 {
-	[Service(nameof(OrdersConsoleModule), typeof(IModule))]
-	class OrdersConsoleModule : IModule
+	[Service(nameof(OrdersConsoleCommand), typeof(IConsoleCommand))]
+	class OrdersConsoleCommand : IConsoleCommand
 	{
 		private readonly IConsole console;
 		private readonly IOrderingService orderingService;
 
-		public OrdersConsoleModule(IConsole console, IOrderingService orderingService)
+		public OrdersConsoleCommand(IConsole console, IOrderingService orderingService)
 		{
 			this.console = console;
 			this.orderingService = orderingService;
 		}
 
-		public void Initialize()
+		public void Execute()
 		{
 			console.WriteLine("OrdersConsole: Show all orders function");
 			string customerName = console.AskInput("Enter customer last name: ");
@@ -28,5 +28,7 @@ namespace ConsoleApplication
 				console.WriteEntity(salesOrderInfo);
 			}
 		}
+
+		public string MenuLabel => "Show all orders";
 	}
 }
