1be0e33e8c1a5df2d3cdf3742de5b85032cf2432
Florin Coros <florin@oncodedesign.com>
2022-11-15 15:30:14 +0200

    Add OrderingService tests

---
+++ AppInfraDemo/Modules/Sales/Sales.Services.UnitTests/OrderingServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Sales.Services.UnitTests
+{
+    [TestClass]
+    public class OrderingServiceTests
+    {
+        [TestMethod]
+        public void GetCustomersWithOrders_MoreOrders_OrderedByStore()
+        {
+            Assert.Fail("Not implemented");
+        }
+    }
+}
---
+++ AppInfraDemo/Modules/Sales/Sales.Services.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sales.Services.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Razer")]
+[assembly: AssemblyProduct("Sales.Services.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Razer 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b9fa65fa-d4f3-45c4-8608-c64cbf69210f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
